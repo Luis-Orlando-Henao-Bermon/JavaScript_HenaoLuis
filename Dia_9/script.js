ada37559d59656b5275a87f812927252b777515c
Luis <roronoazoro3704@gmail.com>
2024-09-02 18:40:19 -0500

    feat :sparkles: day 9 completed

--- Dia_9/script.js
+++ Dia_9/script.js
@@ -1,4 +1,4 @@
-
+var numero;
 document.getElementById('form').addEventListener("submit",function (mostrarPokemon){
     id=document.getElementById('inp').value
     url="https://pokeapi.co/api/v2/pokemon/"+id
@@ -6,22 +6,82 @@ document.getElementById('form').addEventListener("submit",function (mostrarPokem
     fetch(url)
     .then(res=>res.json())
     .then(data=>{
-        document.getElementById('nombre').innerHTML=`<span>${data.id} -</span> ${data.name}`
-        document.getElementById('animacion').innerHTML=`<img src="${data.sprites.other.showdown.front_default}" alt="" id="gif">`
+        if (data.id>919) {
+            
+            document.getElementById('nombre').innerHTML=`<span>${data.id} -</span> ${data.name.toUpperCase()}`
+            document.getElementById('animacion').innerHTML=`<img src="${data.sprites.other["official-artwork"].front_default}" alt="" id="gif">`
+            document.getElementById('audio').innerHTML=`<audio src="${data.cries.latest}" autoplay></audio>`
+        } else{
+            document.getElementById('nombre').innerHTML=`<span>${data.id} -</span> ${data.name.toUpperCase()}`
+            document.getElementById('animacion').innerHTML=`<img src="${data.sprites.other.showdown.front_default}" alt="" id="gif">`
+            document.getElementById('audio').innerHTML=`<audio src="${data.cries.latest}" autoplay></audio>`
 
-        let url2="https://pokeapi.co/api/v2/pokemon/"+(data.id+1)
-        let url3="https://pokeapi.co/api/v2/pokemon/"+(data.id-1)
-        document.getElementById("next").addEventListener("click",next)
-        function next() {
+        }
+
+        numero=data.id
+    })
+})
+document.getElementById("next").addEventListener("click",function (next){
+    next.preventDefault()
+    if (numero<=1024) {
+        if (numero>919) {
+            numero+=1
+            let url2="https://pokeapi.co/api/v2/pokemon/"+numero
             
             fetch(url2)
             .then(res=>res.json())
-            .then(data=>{
-                document.getElementById('nombre').innerHTML=`<span>${data.id} -</span> ${data.name}`
-                document.getElementById('animacion').innerHTML=`<img src="${data.sprites.other.showdown.front_default}" alt="" id="gif">`
+            .then(dataNext=>{
+                document.getElementById('nombre').innerHTML=`<span>${dataNext.id} -</span> ${dataNext.name.toUpperCase()}`
+                document.getElementById('animacion').innerHTML=`<img src="${dataNext.sprites.other["official-artwork"].front_default}" alt="" id="gif">`
+                document.getElementById('audio').innerHTML=`<audio src="${dataNext.cries.latest}" autoplay></audio>`
+            })
+            
+        } else {
+            
+            numero+=1
+            let url2="https://pokeapi.co/api/v2/pokemon/"+numero
+            
+            fetch(url2)
+            .then(res=>res.json())
+            .then(dataNext=>{
+                document.getElementById('nombre').innerHTML=`<span>${dataNext.id} -</span> ${dataNext.name.toUpperCase()}`
+                document.getElementById('animacion').innerHTML=`<img src="${dataNext.sprites.other.showdown.front_default}" alt="" id="gif">`
+                document.getElementById('audio').innerHTML=`<audio src="${dataNext.cries.latest}" autoplay></audio>`
             })
         }
-
-
-    })
+        
+    }
 })
+
+
+document.getElementById("pre").addEventListener("click",function(pre){
+    pre.preventDefault()
+    if (numero>=2) {
+        
+        if (numero>919) {
+            
+            numero-=1
+            let url3="https://pokeapi.co/api/v2/pokemon/"+numero
+            fetch(url3)
+            .then(res=>res.json())
+            .then(dataPre=>{
+                document.getElementById('nombre').innerHTML=`<span>${dataPre.id} -</span> ${dataPre.name.toUpperCase()}`
+                document.getElementById('animacion').innerHTML=`<img src="${dataPre.sprites.other["official-artwork"].front_default}" alt="" id="gif">`
+                document.getElementById('audio').innerHTML=`<audio src="${dataPre.cries.latest}" autoplay></audio>`
+            })
+        } else {
+            numero-=1
+            let url3="https://pokeapi.co/api/v2/pokemon/"+numero
+            fetch(url3)
+            .then(res=>res.json())
+            .then(dataPre=>{
+                document.getElementById('nombre').innerHTML=`<span>${dataPre.id} -</span> ${dataPre.name.toUpperCase()}`
+                document.getElementById('animacion').innerHTML=`<img src="${dataPre.sprites.other.showdown.front_default}" alt="" id="gif">`
+                document.getElementById('audio').innerHTML=`<audio src="${dataPre.cries.latest}" autoplay></audio>`
+            })
+            
+        }
+        
+    }
+})
+//Desarrollado por Luis Henao
